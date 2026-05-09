103fb9d3f383ecb25c11bb5c6a6d19c45969ed00
linyi <jianglinyi97@gmail.com>
2026-05-09 12:18:02 -0400

    fix dockefile

--- TransactionCategorizer/Program.cs
+++ TransactionCategorizer/Program.cs
@@ -4,7 +4,6 @@ using TransactionCategorizer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddSingleton<PiiRedactor>();
 
 builder.Services.Configure<GeminiOptions>(
@@ -22,14 +21,13 @@ builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
+app.UseStatusCodePagesWithReExecute("/not-found");
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
